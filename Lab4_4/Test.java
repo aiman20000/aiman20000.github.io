19effcd78a9a44e4fba140070e4f39d9d6a51804
aiman20000 <94754600+aiman20000@users.noreply.github.com>
2022-02-02 21:51:08 +0300

    Add files via upload

---
+++ Lab4_4/Test.java
@@ -0,0 +1,27 @@
+package Lab4_4;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        circlrlarLinkedList<String> list=new circlrlarLinkedList<>();
+        Scanner in=new Scanner(System.in);
+        int choice=-1;
+        System.out.println("1 add first\n"+
+                "2 add last \n"+
+                "3 remove first \n"+
+                "4 rotate \n"
+        +"0 exit ");
+        while (choice!=0)
+        {
+            System.out.println("Your choice: ");
+            choice=in.nextInt();
+            switch (choice)
+            {
+                case 1:
+            }
+
+
+        }
+    }
+}
